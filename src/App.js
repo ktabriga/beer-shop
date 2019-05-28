d1998809ea8e024f122a70b14a812aece2e8e291
Catabriga <jhonatancatabriga@gmail.com>
2019-05-28 14:47:52 -0300

    Cria componente Title com interpolação

--- src/App.js
+++ src/App.js
@@ -2,23 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Title extends React.Component {
+  title = 'Beer Shop'
+
+  render() {
+    return (
+      <h1 className='Title'>{this.title}</h1>
+    )
+  }
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title />
     </div>
   );
 }
